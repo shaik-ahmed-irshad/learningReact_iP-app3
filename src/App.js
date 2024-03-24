c27bc2154eb38352d4fe4ad86d04918db77b647d
shaik-ahmed-irshad <edventurous.ahmed@gmail.com>
2024-03-24 16:26:20 +0530

    getting hang how to diaplay backend data in tabular form Using Reactgit add .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 import './App.css';
 
 function App() {
+  const [data,setData] = useState([])
+
+  useEffect(()=>{
+    const getData = async ()=>{
+      try {
+        let res = await axios.get('https://jsonplaceholder.typicode.com/users')
+        setData(res.data)
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getData();
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <center>
+          <table>
+            <thead>
+              <tr>
+                <th rowspan="2">ID</th>
+                <th rowspan="2">Name</th>
+                <th rowspan="2">Username</th>
+                <th rowspan="2">Email</th>
+                <th rowspan="2">Address</th>
+                <th colspan="2">Geo</th>
+                <th rowspan="2">Phone</th>
+                <th rowspan="2">Website</th>
+                <th rowspan="2">Company</th>
+              </tr>
+              <tr>
+                <th>Lat</th>
+                <th>Lng</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((ele) => (
+                <tr key={ele.id}>
+                  <td>{ele.id}</td>
+                  <td>{ele.name}</td>
+                  <td>{ele.username}</td>
+                  <td>{ele.email}</td>
+                  <td>{`${ele.address.street}, ${ele.address.city}`}</td>
+                  <td>{`${ele.address.geo.lat}`}</td>
+                  <td>{`${ele.address.geo.lng}`}</td>
+                  <td>{ele.phone}</td>
+                  <td>{ele.website}</td>
+                  <td>{ele.company.name}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+      </center>
+    </>
   );
 }
+
 
 export default App;
