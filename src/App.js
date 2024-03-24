0a88a8910b602bed618be9ccb18fcb24245fe4f3
shaik-ahmed-irshad <edventurous.ahmed@gmail.com>
2024-03-24 17:57:25 +0530

    loops & functions in <td>{ }</td>

--- src/App.js
+++ src/App.js
@@ -1,63 +1,87 @@
-import {useState, useEffect} from 'react'
-import axios from 'axios'
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Loading from "./Loading";
+import "./App.css";
 
 function App() {
-  const [data,setData] = useState([])
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  useEffect(()=>{
-    const getData = async ()=>{
+  useEffect(() => {
+    const getData = async () => {
       try {
-        let res = await axios.get('https://jsonplaceholder.typicode.com/users')
-        setData(res.data)
+        setLoading(true);
+
+        let { data } = await axios.get(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        setLoading(false);
+        setData(data);
       } catch (error) {
         console.error(error);
       }
-    }
+    };
     getData();
-  },[])
+  }, []);
+
+  const extractAddress = (obj) => {
+    let arr = Object.values(obj);
+    let elems = [];
+    for (let i = 0; i < arr.length - 1; i++) {
+      elems.push(<p key={i}>{arr[i]}</p>);
+    }
+    return elems;
+  };
   return (
     <>
       <center>
-          <table>
-            <thead>
-              <tr>
-                <th rowspan="2">ID</th>
-                <th rowspan="2">Name</th>
-                <th rowspan="2">Username</th>
-                <th rowspan="2">Email</th>
-                <th rowspan="2">Address</th>
-                <th colspan="2">Geo</th>
-                <th rowspan="2">Phone</th>
-                <th rowspan="2">Website</th>
-                <th rowspan="2">Company</th>
+        <table>
+          {loading && <Loading />}
+          <thead>
+            <tr>
+              <th rowspan="2">ID</th>
+              <th rowspan="2">Name</th>
+              <th rowspan="2">Username</th>
+              <th rowspan="2">Email</th>
+              <th rowspan="2">Address</th>
+              <th colspan="2">Geo</th>
+              <th rowspan="2">Phone</th>
+              <th rowspan="2">Website</th>
+              <th rowspan="2">Company</th>
+            </tr>
+            <tr>
+              <th>Lat</th>
+              <th>Lng</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((ele) => (
+              <tr key={ele.id}>
+                <td>{ele.id}</td>
+                <td>{ele.name}</td>
+                <td>{ele.username}</td>
+                <td>{ele.email}</td>
+                {/* <td>{`${ele.address.street}, ${ele.address.city}`}</td> */}
+                <td>{extractAddress(ele.address)}</td>
+                <td>{`${ele.address.geo.lat}`}</td>
+                <td>{`${ele.address.geo.lng}`}</td>
+                <td>{ele.phone}</td>
+                <td>
+                  <a href={ele.website}> {ele.website} </a>
+                </td>
+                {/* <td>{ele.company.name}</td> */}
+                <td>
+                  {Object.values(ele.company).map((ele, i) => (
+                    <p key={i}>{ele}</p>
+                  ))}
+                </td>
               </tr>
-              <tr>
-                <th>Lat</th>
-                <th>Lng</th>
-              </tr>
-            </thead>
-            <tbody>
-              {data.map((ele) => (
-                <tr key={ele.id}>
-                  <td>{ele.id}</td>
-                  <td>{ele.name}</td>
-                  <td>{ele.username}</td>
-                  <td>{ele.email}</td>
-                  <td>{`${ele.address.street}, ${ele.address.city}`}</td>
-                  <td>{`${ele.address.geo.lat}`}</td>
-                  <td>{`${ele.address.geo.lng}`}</td>
-                  <td>{ele.phone}</td>
-                  <td>{ele.website}</td>
-                  <td>{ele.company.name}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+            ))}
+          </tbody>
+        </table>
       </center>
     </>
   );
 }
-
 
 export default App;
